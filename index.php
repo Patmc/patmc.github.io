3c313b174df82d81153558bacfb2668827f8dea1
Patmc <pmclarnon91@gmail.com>
2015-12-12 22:10:48 +0000

    Initial Upload

---
+++ index.php
@@ -0,0 +1,221 @@
+<?php include 'header.php' ?>
+
+<span id="home" class="anchor"></span>
+<div id="homebar" class="container-fluid site-table" style="background-image:url('./images/bg_wall.jpg');">
+
+<div style="background-image:url('./images/bg_wall.jpg');" class="before"></div>
+<div style="background: rgba(0, 0, 0, 0.2) none repeat scroll 0% 0%;" class="site-wrapper-inner" id="home-content">
+  <div style="margin-top: 20px;" id="home-text-wrapper">
+    <div id="home-text">
+      <h1>Paddy McLarnon</h1>
+      <hr class="homehr"/>
+      <h2>Web Developer</h2>
+    </div>
+  </div>
+</div>
+</div>
+
+
+<div class="container-fluid work" id="work">	
+	<div class="container top-buffer">
+		<span id="projects" class="anchor"></span>
+		<h2>Some of my work!</h2>
+		<br />
+		<div class="projectbox col-md-15">
+			
+			<a href="#"><div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+				<div class="flipper">
+					<div class="front">
+						<img src="images/agmain/aghome-circle.png" class="image-cropper">
+					</div>
+					<div class="back">
+						<img src="images/agmain/aghome-circle-bk.png" class="image-cropper">
+					</div>
+				</div>
+			</div>
+			<h3 class="text-center">AG</h3></a>
+		</div>
+
+		<div class="projectbox col-md-15">
+			<a href="#"><div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+				<div class="flipper">
+					<div class="front">
+						<img src="images/aghome/orhome-circle.png" class="image-cropper">
+					</div>
+					<div class="back">
+						<img src="images/aghome/orhome-circle-bk.png" class="image-cropper">
+					</div>
+				</div>
+			</div>
+			<h3 class="text-center">Outside Rooms</h3></a>
+
+		</div>
+
+		<div class="projectbox col-md-15">
+			<a href="#"><div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+				<div class="flipper">
+					<div class="front">
+						<img src="images/shake/shake_home-circle.png" class="image-cropper">
+					</div>
+					<div class="back">
+						<img src="images/shake/shake_home-circle-bk.png" class="image-cropper">
+					</div>
+				</div>
+			</div>
+			<h3 class="text-center">Shakespeare's Plays</h3></a>
+		</div>
+
+		<div class="projectbox col-md-15">
+			<a href="#"><div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+				<div class="flipper">
+					<div class="front">
+						<img src="images/vision/visionhome-circle.png" class="image-cropper">
+					</div>
+					<div class="back">
+						<img src="images/vision/visionservices-circle.png" class="image-cropper">
+					</div>
+				</div>
+			</div>
+			<h3 class="text-center">Vision Carpentry</h3></a>			
+		</div>
+
+		<div class="projectbox col-md-15">
+			<a href="#"><div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+				<div class="flipper">
+					<div class="front">
+						<img src="images/imdshare/imdhome-circle.png" class="image-cropper">
+					</div>
+					<div class="back">
+						<img src="images/imdshare/imdpost-circle.png" class="image-cropper">
+					</div>
+				</div>
+			</div>
+			<h3 class="text-center">IMDShare</h3></a>			
+		</div>
+
+	</div>
+
+</div>
+
+<div class="container">
+
+	<div class="row top-buffer">
+	<span id="cv" class="anchor"></span>
+		<div class="education col-md-5">
+		
+			<h2>Education</h2>
+
+			<h4><strong>St Mary’s Grammar School, Magherafelt</strong><small class="text-muted pull-right">2002-2009</small></h4>
+			<ul>
+			    <li><strong>10 GCSE:</strong> Grades A-C, Including IT, Science, Maths and English</li>
+			    <li><strong>A-Level's:</strong> Grade A in ICT, Grade B in Economics, Grade B in Geography</li>
+			</ul>
+			<br/>
+
+			<h4><strong>University of Ulster, Jordanstown</strong><small class="text-muted pull-right">2009-2013</small></h4>
+			<ul>
+				<li>BSC Hons Interactive Multimedia Design</li> 
+			</ul>
+			<br/>
+
+			<h4><strong>University of Ulster</strong><small class="text-muted pull-right">2013-2014</small></h4>
+			<ul>
+				<li>Advanced Diploma in Management Practice</li>
+			</ul>
+			<br/>
+
+			<h2 class="top-buffer">Skills</h2>
+			<div class="skills col-md-3">
+				<ul>
+					<li>HTML</li>
+					<li>HTML5</li>
+					<li>CSS</li>
+					<li>CSS3</li>
+				</ul>
+			</div>
+			<div class="skills col-md-3">
+				<ul>
+					<li>Sass</li>
+					<li>LESS</li>
+					<li>Bootstrap</li>			
+					<li>XML</li>
+				</ul>
+			</div>
+			<div class="skills col-md-3">
+				<ul>
+					<li>xHTML</li>
+					<li>JavaScript</li>
+					<li>Angularjs</li>
+					<li>jQuery</li>
+				</ul>
+			</div>
+			<div class="skills col-md-3">
+				<ul>
+					<li>PHP</li>
+					<li>MySQL</li>
+					<li>ActionScript</li>
+					<li>Git</li>
+				</ul>
+			</div>
+
+		</div>
+
+		<div class="col-md-1">
+		</div>
+
+		<div class="experience col-md-6">
+			
+			<h2>Experience</h2>
+
+		    <ul class="timeline">
+		        <li>
+					<div class="timeline-badge primary"><i class="glyphicon glyphicon-briefcase"></i></div>
+					<div class="timeline-panel">
+						<div class="timeline-heading">
+							<h4 class="timeline-title">Acheson & Glover</h4>
+							<p><small class="text-muted">October 2013 - November 2015</small></p>
+						</div>
+						<div class="timeline-body">
+							<!--<p>Mussum ipsum cacilds, vidis litro abertis.</p>-->
+						</div>
+					</div>
+		        </li>
+		        <li class="timeline-inverted">
+		        	<div class="timeline-badge success"><i class="glyphicon glyphicon-briefcase"></i></div>
+		         	<div class="timeline-panel">
+		        		<div class="timeline-heading">
+		              		<h4 class="timeline-title">Frank Idea Works</h4>
+		              		<p><small class="text-muted">May - July 2012</small></p>
+		            	</div>
+		            	<div class="timeline-body">
+		              		<!--<p>I did a few weeks work experience with Frank Idea Works (now Big Motive) after I completed my placement year at LeasePlan IS. Here I got some valuable experience working at an agency creating HTML websites from wireframes.</p>-->
+		           		</div>
+		          	</div>
+		        </li>
+		        <li>
+		          	<div class="timeline-badge info"><i class="glyphicon glyphicon-briefcase"></i></div>
+		         	<div class="timeline-panel">
+		            	<div class="timeline-heading">
+		              		<h4 class="timeline-title">LeasePlan Infrastructure Services</h4>
+		              		<p><small class="text-muted">August 2011 – April 2012</small></p>
+		            	</div>
+		            	<div class="timeline-body">
+		              		<!--<p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis.</p>-->
+		            	</div>
+		          	</div>
+		        </li>
+		    </ul>
+		</div>
+	</div>
+	<div class="row top-seperator">
+
+		<div class="skills col-md-6">
+			<h2>What Next?</h2>
+			<p>I'm interested in taking a look at a few different languages, particularly ruby and .asp. The CMS 'Umbraco' has me wanting to get into asp more because it looks pretty interesting.</p> 
+		</div>
+
+	</div>
+
+</div>
+
+<?php include 'footer.php' ?>
---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    //caches a jQuery object containing the header element
+    var header = $(".clearHeader");
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 001) {
+            header.removeClass('clearHeader').addClass("darkHeader");
+        } else {
+            header.removeClass("darkHeader").addClass('clearHeader');
+        }
+    });
+});
+
+var $root = $('html, body');
+$('a').click(function() {
+    var href = $.attr(this, 'href');
+    $root.animate({
+        scrollTop: $(href).offset().top
+    }, 500, function () {
+        window.location.hash = href;
+    });
+    return false;
+});
